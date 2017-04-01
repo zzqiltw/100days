f0fb445d893c2d78d38d430bbefaad2474eae8e4
zzqiltw <hzzhengzhiqin@corp.netease.com>
2017-04-01 14:54:09 +0800

    feat : add visiable imageview

--- 100days/ZQPageCollectionViewCell.m
+++ 100days/ZQPageCollectionViewCell.m
@@ -10,11 +10,15 @@
 #import "UIImage+ZQ.h"
 #import <Masonry/Masonry.h>
 
+static CGFloat const kZQPageImageViewWH = 140;
+
 @interface ZQPageCollectionViewCell()
 
 @property (nonatomic, strong) UILabel *titleLabel;
 @property (nonatomic, strong) UILabel *detailLabel;
 @property (nonatomic, strong) UIImageView *bgImageView;
+
+@property (nonatomic, strong) UIImageView *visiableImageView;
 
 @end
 
@@ -29,6 +33,10 @@
         
         [self detailLabel];
         
+        [self visiableImageView];
+        
+        self.layer.shouldRasterize = YES;
+        self.layer.rasterizationScale = [UIScreen mainScreen].scale;
     }
     return self;
 }
@@ -38,6 +46,7 @@
     self.titleLabel.text = pageModel.title;
     self.detailLabel.text = pageModel.detail;
     self.bgImageView.image = [pageModel.image stackBlur:70];
+    self.visiableImageView.image = pageModel.image;
 }
 
 - (UILabel *)titleLabel
@@ -45,7 +54,7 @@
     if (!_titleLabel) {
         _titleLabel = [UILabel new];
         
-        _titleLabel.textColor = [UIColor redColor];
+//        _titleLabel.textColor = [UIColor redColor];
         _titleLabel.font = [UIFont systemFontOfSize:15];
         
         [self.contentView addSubview:_titleLabel];
@@ -53,7 +62,7 @@
         [_titleLabel mas_makeConstraints:^(MASConstraintMaker *make) {
             make.centerX.offset(0);
             
-            make.top.offset(30);
+            make.top.equalTo(self.visiableImageView.mas_bottom).offset(30);
         }];
     }
     return _titleLabel;
@@ -64,7 +73,7 @@
     if (!_detailLabel) {
         _detailLabel = [UILabel new];
         
-        _detailLabel.textColor = [UIColor redColor];
+//        _detailLabel.textColor = [UIColor redColor];
         _detailLabel.font = [UIFont systemFontOfSize:16];
         
         [self.contentView addSubview:_detailLabel];
@@ -90,5 +99,28 @@
         }];
     }
     return _bgImageView;
+}
+
+- (UIImageView *)visiableImageView
+{
+    if (!_visiableImageView) {
+        _visiableImageView = [[UIImageView alloc] init];
+        
+        _visiableImageView.layer.borderWidth = 2.f;
+        _visiableImageView.layer.masksToBounds = YES;
+        _visiableImageView.layer.cornerRadius = kZQPageImageViewWH * 0.5;
+        _visiableImageView.layer.borderColor = [UIColor whiteColor].CGColor;
+        
+        [self.contentView addSubview:_visiableImageView];
+        
+        [_visiableImageView mas_makeConstraints:^(MASConstraintMaker *make) {
+            make.top.offset(100);
+            
+            make.centerX.offset(0);
+            
+            make.width.height.equalTo(@(kZQPageImageViewWH));
+        }];
+    }
+    return _visiableImageView;
 }
 @end
